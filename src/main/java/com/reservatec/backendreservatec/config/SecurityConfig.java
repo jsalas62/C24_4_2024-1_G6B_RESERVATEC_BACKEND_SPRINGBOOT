ec9c223908e246a1d6f3ca246114e3d3f587c314
Jorge Salas <jsalopdev@gmail.com>
2024-06-26 21:59:01 -0500

    console

--- src/main/java/com/reservatec/backendreservatec/config/SecurityConfig.java
+++ src/main/java/com/reservatec/backendreservatec/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.reservatec.backendreservatec.config;
 
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +12,7 @@ import org.springframework.security.web.authentication.SimpleUrlAuthenticationSu
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 import java.util.Arrays;
 
 @Configuration
