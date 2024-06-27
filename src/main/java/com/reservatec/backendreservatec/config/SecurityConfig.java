e68d78044ca324ab905496b3c59efdc831caae2b
Jorge Salas <jsalopdev@gmail.com>
2024-06-27 16:10:41 -0500

    prop_prod

--- src/main/java/com/reservatec/backendreservatec/config/SecurityConfig.java
+++ src/main/java/com/reservatec/backendreservatec/config/SecurityConfig.java
@@ -4,15 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -20,32 +24,37 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        SimpleUrlAuthenticationSuccessHandler successHandler = new SimpleUrlAuthenticationSuccessHandler();
+        successHandler.setDefaultTargetUrl("http://localhost:3000");
+
         return http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/**").permitAll() // Permitir acceso público a todas las rutas
-                )
-                .oauth2Login(oauth2 -> oauth2
-                        .loginPage("/login")  // Página de inicio de sesión personalizada
-                        .defaultSuccessUrl("http://localhost:3000/home", true) // URL de redirección después del inicio de sesión exitoso
-                )
-
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/public/**").permitAll(); // Permitir acceso a rutas públicas
+                    auth.requestMatchers("/favicon.ico").permitAll(); // Permitir acceso a /favicon.ico
+                    auth.anyRequest().authenticated(); // Cualquier otra solicitud requiere autenticación
+                })
                 .sessionManagement(session -> session
-                        //.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                        .sessionFixation(SessionManagementConfigurer.SessionFixationConfigurer::migrateSession)
+                        .invalidSessionUrl("/login?error=invalid_session")
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
                         .maximumSessions(1)
                         .sessionRegistry(sessionRegistry())
-                        .expiredUrl("/login?error=session_expired"))
+                        .expiredUrl("/login?error=session_expired")
+                        .maxSessionsPreventsLogin(false)
+                )
+                .csrf(csrf -> csrf.disable())
                 .logout(logout -> logout
                         .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login")
+                        .logoutSuccessUrl("/login?logout")
                         .deleteCookies("JSESSIONID")
                         .invalidateHttpSession(true)
-                        .clearAuthentication(true))
-                .csrf(csrf -> csrf.disable())
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .defaultSuccessUrl("http://localhost:3000/home", true)
-                        .permitAll())
+                        .clearAuthentication(true)
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .successHandler(successHandler)
+                )
+                .formLogin(withDefaults())
                 .build();
     }
 
