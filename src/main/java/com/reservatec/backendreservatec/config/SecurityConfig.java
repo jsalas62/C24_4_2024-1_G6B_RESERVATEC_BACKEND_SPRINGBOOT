fcf6c1aabc03eed04824c80d1816a3e2a61d1dfd
Jorge Salas <jsalopdev@gmail.com>
2024-06-27 19:42:52 -0500

    delete_same_cookies

--- src/main/java/com/reservatec/backendreservatec/config/SecurityConfig.java
+++ src/main/java/com/reservatec/backendreservatec/config/SecurityConfig.java
@@ -71,12 +71,5 @@ public class SecurityConfig {
     public SessionRegistry sessionRegistry() {
         return new SessionRegistryImpl();
     }
-
-    @Bean
-    public DefaultCookieSerializer cookieSerializer() {
-        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
-        serializer.setSameSite("None");
-        serializer.setUseSecureCookie(true); // Asegúrate de que esto esté en true si usas HTTPS
-        return serializer;
-    }
+    
 }
