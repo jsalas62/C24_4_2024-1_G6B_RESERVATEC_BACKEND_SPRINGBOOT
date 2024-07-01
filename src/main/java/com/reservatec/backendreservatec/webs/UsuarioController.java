a348bee1e8bb51104f50897549860202fd1e1d63
jsalop <jsalop@jsalop-dev.local>
2024-07-01 12:28:26 -0500

    railway

--- src/main/java/com/reservatec/backendreservatec/webs/UsuarioController.java
+++ src/main/java/com/reservatec/backendreservatec/webs/UsuarioController.java
@@ -43,7 +43,7 @@ public class UsuarioController {
         String email = token.getPrincipal().getAttribute("email");
         boolean usuarioRegistrado = usuarioService.existsByEmail(email);
 
-        String redirectUrl = usuarioRegistrado ? "https://balanced-delight-production.up.railway.app/inicio" : "https://balanced-delight-production.up.railway.app/register";
+        String redirectUrl = usuarioRegistrado ? "https://strong-laughter-production.up.railway.app/inicio" : "https://strong-laughter-production.up.railway.app/register";
         return ResponseEntity.status(HttpStatus.FOUND).header("Location", redirectUrl).build();
     }
 
